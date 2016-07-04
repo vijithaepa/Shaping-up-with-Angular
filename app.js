d391321641f60c3e15b21c0cde73c6ce5b5f3e22
vijitha <vijithaepa@gmail.com>
2016-07-04 15:43:04 +1000

    updates

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 (function() {
-  var app = angular.module('gemStore',['product-controller','product-directive']);
+  var app = angular.module('gemStore',['product-directive']);
 
+    app.controller('StoreController', function($http, $log){
+        var store = this;
+        store.products = [];
+
+        $http.get('http://localhost:63342/Shaping-up-with-Angular/store.json')
+            .success(function(data){
+                $log.info(data);
+                store.products = data.store;
+            });
+    });
+
+    app.controller("ReviewController", function(){
+        this.review = {};
+        this.review.createdOn = Date.now();
+
+        this.addReview = function(product){
+            product.reviews.push(this.review);
+            this.review = {};
+        };
+    });
 
 })();
